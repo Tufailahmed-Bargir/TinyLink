7c624a23d0806e6f7113f900074af4f356c4db3e
Tufailahmed <bargirtufailahmed@gmail.com>
2025-12-05 12:19:14 +0530

    feat:implemnt the link stat functionality

--- app/api/links/[code]/route.ts
+++ app/api/links/[code]/route.ts
@@ -30,3 +30,46 @@ export async function DELETE(
 }
   
   }
+
+
+  export async function GET(
+  request: NextRequest,
+  { params }: { params: { code: string } }
+//   @ts-ignore
+): Promise<NextResponse> { // Add explicit return type Promise<NextResponse>
+  const { code } =await  params;
+  console.log('code is', code);
+
+    try{
+   const linkStat =  await prisma.link.findUnique({
+    where:{
+        code
+    }, select: {
+        code: true,
+        targetUrl: true,
+        totalClicks: true,  
+        updatedAt:true,
+        createdAt: true,
+      },
+  }
+
+)
+
+if (!linkStat) {
+      return NextResponse.json({ error: `Link with code '${code}' not found.` }, { status: 404 });
+    }
+
+   
+    return NextResponse.json({ 
+      data: linkStat 
+    }, { status: 200 });
+
+ 
+}catch(e){
+    return NextResponse.json({
+        msg:"error found!",
+        error:e.message
+    })
+}
+  
+  }
